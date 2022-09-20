0aed0da22488c9597f4ace654204f48338d2c166
m-Murakami1313 <mituvv@gmail.com>
2022-09-20 20:23:37 +0900

    fix useFetchAPIDataをテストしやすくリファクタリングした

--- src/hooks/useFetchAPIDATA.ts
+++ src/hooks/useFetchAPIDATA.ts
@@ -11,28 +11,29 @@ export const useFetchAPIData = () => {
       const deleteData = newData.filter((data) => data.prefCode !== e.target.id)
       setCheckedPrefectures(deleteData)
     } else {
-      fetchPrectureData(e, newData)
+      const fetchData = async () => {
+        const data = await fetchPrectureData(e)
+        setCheckedPrefectures([
+          ...newData,
+          { prefName: e.target.value, prefCode: e.target.id, prefData: data?.result.data },
+        ])
+      }
+      fetchData()
     }
   }
 
-  const fetchPrectureData = async (
-    e: React.ChangeEvent<HTMLInputElement>,
-    newData: checkedPrefecturesTypes[],
-  ) => {
+  const fetchPrectureData = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const url = `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=${e.target.id}`
     try {
       const response = await fetch(url, {
         headers: { 'x-api-key': process.env.NEXT_PUBLIC_RESAS_API } as HeadersInit,
       })
       const data: prefecturesDataTypes = await response.json()
-      setCheckedPrefectures([
-        ...newData,
-        { prefName: e.target.value, prefCode: e.target.id, prefData: data.result.data },
-      ])
+      return data
     } catch (error) {
       console.log('情報の取得に失敗しました')
     }
   }
 
-  return { handleCheckData, checkedPrefectures }
+  return { handleCheckData, checkedPrefectures, fetchPrectureData }
 }
